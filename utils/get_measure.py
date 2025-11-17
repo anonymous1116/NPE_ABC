5b727b6356f8d1b57132bab49f9e94b888bfb17c
anonymous1116 <hhyun116@gmail.com>
2025-11-17 17:36:13 -0500

    NPE_run

--- utils/get_measure.py
+++ utils/get_measure.py
@@ -30,7 +30,7 @@ def run_similiarity(task, measure, x0_ind, seed, post_n_samples, num_training, c
     x0 = torch.tensor(x0, dtype = torch.float32)
     if x0.ndim == 1:
         x0= torch.reshape(x0, (1, x0.size(0)))
-    sample_post = posterior((post_n_samples,), x=torch.tensor(x0))
+    sample_post = posterior.sample((post_n_samples,), x=torch.tensor(x0))
 
     if measure == "c2st":
         sample_post_size = sample_post.size(0)
