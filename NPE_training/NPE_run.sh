34f3d2ab68d79d6bea969c9960606831adefb2a4
anonymous1116 <hhyun116@gmail.com>
2025-11-19 11:46:45 -0500

    calibrating.py

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=cpu
 #SBATCH --account=statdept
-#SBATCH --time=04:00:00
-#SBATCH --qos=standby
+#SBATCH --time=1-04:00:00
+#SBATCH --qos=normal
 #SBATCH --array=1-10               # Create a job array with indices from 1 to 10
 #SBATCH --output=NPE/NPE_nsf/output_log/output_log_%A_%a.log
 #SBATCH --error=NPE/NPE_nsf/output_log/error_log_%A_%a.txt
@@ -30,7 +30,7 @@ seeds=$((seed_START + SLURM_ARRAY_TASK_ID - 1))
 
 # Run the Python script with the specified N_EPOCHS value
 echo "Running with seed=$seeds"
-python NPE_training/NPE_run.py --task "bernoulli_glm2" --seed $seeds --cond_den "nsf" --num_training 100000
+python NPE_training/NPE_run.py --task "bernoulli_glm2" --seed $seeds --cond_den "nsf" --num_training 500000
 #python NPE_training/NPE_run.py --task "bernoulli_glm2" --seed 1 --num_training 1000 --cond_den "nsf"
 echo "## Run Completed for seed=$seeds ##"
 
