73e62119869ea18a6d96161d05ee463c0fe3e78b
anonymous1116 <hhyun116@gmail.com>
2025-11-19 15:28:41 -0500

    calibrating.py

---
+++ NPE_training/SNPE_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=cpu
+#SBATCH --account=statdept
+#SBATCH --time=04:00:00
+#SBATCH --qos=standby
+#SBATCH --array=0-99               # Create a job array with indices from 1 to 10
+#SBATCH --output=NPE/NPE_nsf/output_log/output_log_%A_%a.log
+#SBATCH --error=NPE/NPE_nsf/output_log/error_log_%A_%a.txt
+
+# Create the output_log directory if it doesn't exist
+mkdir -p NPE/NPE_nsf/output_log
+
+# Load the required Python environment
+module load conda
+conda activate /depot/wangxiao/apps/hyun18/NPE_NABC
+
+# Change to the directory where the job was submitted from
+SLURM_SUBMIT_DIR=/home/hyun18/NPE_ABC
+cd $SLURM_SUBMIT_DIR
+
+# Calculate seed and dim_out
+seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
+# Run the calibrate_amor.py
+x0_ind=$((SLURM_ARRAY_TASK_ID % 10)) 
+
+echo "[$(date)] Starting job: x0_ind=$x0_ind, seed=$seed, L=$L"
+python NPE_training/SNPE_run.py --task "two_moons" --seed $seeds --x0_ind $x0_ind --cond_den "nsf" --num_training 1000
+echo "[$(date)] Job complete: x0_ind=$x0_ind, seed=$seed"
