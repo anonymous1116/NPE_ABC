78bff4acf2dbfad7c677edc5ed418446add280c3
anonymous1116 <hhyun116@gmail.com>
2025-12-08 12:52:43 -0500

    calibrating.py

--- NPE_training/SNPE_run.sh
+++ NPE_training/SNPE_run.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --partition=cpu
 #SBATCH --account=statdept
-#SBATCH --time=3-04:00:00
+#SBATCH --time=13-04:00:00
 #SBATCH --qos=normal
 #SBATCH --array=0-99               # Create a job array with indices from 1 to 10
 #SBATCH --output=SNPE/NPE_nsf/output_log/output_log_%A_%a.log
@@ -25,7 +25,7 @@ cd $SLURM_SUBMIT_DIR
 seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 # Run the calibrate_amor.py
 x0_ind=$((SLURM_ARRAY_TASK_ID % 10)) 
-num_training=200000
+num_training=100000
 task="bernoulli_glm2"
 total_round=10
 echo "[$(date)] Starting job: x0_ind=$x0_ind, seed=$seed, L=$L"
@@ -33,8 +33,3 @@ python NPE_training/SNPE_run.py --task $task --seed $seed --x0_ind $x0_ind --num
 #python NPE_training/SNPE_run_seq.py --task $task --seed $seed --x0_ind $x0_ind --num_training $num_training --cond_den "nsf" --total_round $total_round
 
 echo "[$(date)] Job complete: x0_ind=$x0_ind, seed=$seed"
-
-
-#python NPE_training/SNPE_run_seq.py --task "two_moons" --seed 1 --x0_ind 1 --num_training 1000 --cond_den "nsf" --total_round 5
-#python NPE_training/SNPE_run.py --task "two_moons" --seed 1 --x0_ind 1 --num_training 1000 --cond_den "nsf" --total_round 5
-
