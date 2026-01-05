a0d6365bee56c6a6580b7e31f98e2fd4159b7a78
anonymous1116 <hhyun116@gmail.com>
2026-01-04 19:29:39 -0500

    PTrans2.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=1-01:29:00
+#SBATCH --time=00:29:00
 #SBATCH --account=statdept
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=170G
 #SBATCH --qos=normal
-#SBATCH --partition=
+#SBATCH --partition=a10,a100-80gb
 #SBATCH --array=0-99
 #SBATCH --output=ABC_calibration/log/output_log_%A_%a.out
 #SBATCH --error=ABC_calibration/log/error_log_%A_%a.txt
@@ -29,7 +29,7 @@ seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
 L=100000000
 task="bernoulli_glm2"
-num_training=100000
+num_training=300000
 tol=1e-4
 
 # Run the calibrate_amor.py
