54539effa931be40e5b834e0b744b36dec4aa539
anonymous1116 <hhyun116@gmail.com>
2026-01-07 16:02:31 -0500

    PTrans2.py

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -30,7 +30,7 @@ seeds=$((seed_START + SLURM_ARRAY_TASK_ID - 1))
 
 # Run the Python script with the specified N_EPOCHS value
 echo "Running with seed=$seeds"
-python NPE_training/NPE_run.py --task "double_slcp_summary_transform2" --seed $seeds --cond_den "nsf" --num_training 2000000
+python NPE_training/NPE_run.py --task "double_slcp_summary_transform2" --seed $seeds --cond_den "nsf" --num_training 3000000
 #python NPE_training/NPE_run.py --task "double_slcp_summary_transform2" --seed 1 --num_training 10000 --cond_den "nsf"
 #python NPE_training/SNPE_run.py --task "two_moons" --seed 1 --num_training 1000 --cond_den "nsf" --x0_ind 0
 echo "## Run Completed for seed=$seeds ##"
