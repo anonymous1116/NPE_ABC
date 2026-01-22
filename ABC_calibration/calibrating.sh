eb50f5868d986a6cdc00174089964bcfe85b41a9
anonymous1116 <hhyun116@gmail.com>
2026-01-21 21:58:53 -0500

    PTrans2.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -27,10 +27,10 @@ cd $SLURM_SUBMIT_DIR
 # Calculate seed and dim_out
 seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
-L=10000000
+L=100000000
 task="my_five_twomoons"
 num_training=2000000
-tol=1e-3
+tol=1e-4
 
 # Run the calibrate_amor.py
 x0_ind=$((SLURM_ARRAY_TASK_ID % 10)) 
