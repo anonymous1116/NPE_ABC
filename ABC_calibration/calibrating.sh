2024d5121afc77721c1c2f082eaf2b49ba0a61f8
anonymous1116 <hhyun116@gmail.com>
2026-01-22 11:53:04 -0500

    PTrans2.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=00:29:00
+#SBATCH --time=00:09:00
 #SBATCH --account=statdept
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=170G
@@ -27,10 +27,10 @@ cd $SLURM_SUBMIT_DIR
 # Calculate seed and dim_out
 seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
-L=100000000
+L=10000000
 task="my_five_twomoons"
 num_training=500000
-tol=1e-4
+tol=1e-3
 
 # Run the calibrate_amor.py
 x0_ind=$((SLURM_ARRAY_TASK_ID % 10)) 
