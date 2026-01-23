455e7690f2be16a1ccd806385dd2d716d3e1d688
anonymous1116 <hhyun116@gmail.com>
2026-01-23 09:45:54 -0500

    embedding

--- NPE_training/NPE_run_embed.py
+++ NPE_training/NPE_run_embed.py
@@ -48,7 +48,7 @@ def main(args):
     X = simulators(theta)
 
     #embedding_net = EmbeddingNet(x_dim = X.size(1), c_dim = theta.size(1))
-    embedding_net =SelectFirst10()
+    embedding_net = SelectFirst10()
 
     neural_posterior = posterior_nn(model=args.cond_den, embedding_net=embedding_net)
     # Create inference object
