8e51afd64302c2b6fc551319d72b232b2fa1ad03
anonymous1116 <hhyun116@gmail.com>
2026-01-23 09:19:10 -0500

    embedding

--- NPE_training/SNPE_run.sh
+++ NPE_training/SNPE_run.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=statdept
 #SBATCH --time=4-00:00:00
 #SBATCH --qos=normal
-#SBATCH --array=28,64-99               # Create a job array with indices from 1 to 10
+#SBATCH --array=49-63               # Create a job array with indices from 1 to 10
 #SBATCH --output=SNPE/NPE_nsf/output_log/output_log_%A_%a.log
 #SBATCH --error=SNPE/NPE_nsf/output_log/error_log_%A_%a.txt
 
