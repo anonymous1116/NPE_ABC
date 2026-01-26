895f0aed3e822a2f25e4cbd2cbc2a6393dd2c5f8
anonymous1116 <hhyun116@gmail.com>
2026-01-26 12:47:04 -0500

    PTrans2.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -28,7 +28,7 @@ cd $SLURM_SUBMIT_DIR
 seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
 L=1000000000
-task="my_five_twomoons_err5"
+task="my_five_twomoons_err10"
 num_training=3000000
 tol=1e-5
 
