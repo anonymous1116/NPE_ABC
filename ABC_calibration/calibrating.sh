044e72451818735fffa1f0f5b38687a4b6e6dc9d
anonymous1116 <hhyun116@gmail.com>
2026-01-26 14:05:52 -0500

    PTrans2.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -5,8 +5,8 @@
 #SBATCH --account=statdept
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=170G
-#SBATCH --qos=standby
-#SBATCH --partition=a10,a100-80gb
+#SBATCH --qos=normal
+#SBATCH --partition=a10
 #SBATCH --array=0-99
 #SBATCH --output=ABC_calibration/log/output_log_%A_%a.out
 #SBATCH --error=ABC_calibration/log/error_log_%A_%a.txt
