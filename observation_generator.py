6840d08eed1d6ca842c71e1bc8c1bcc3d6dfe676
anonymous1116 <hhyun116@gmail.com>
2026-04-14 10:36:47 -0400

    Asdf

--- observation_generator.py
+++ observation_generator.py
@@ -10,7 +10,7 @@ def main(args):
         torch.manual_seed(2826)
         x0_list = []
         for j in range(10):
-            sample = np.random.choice(np.arange(1, 11), size=10, replace=True)
+            sample = np.random.choice(np.arange(0, 10), size=10, replace=True)
             tmp = observation_lists("my_twomoons")
             tmp = np.array(tmp)
             print(tmp[sample])
