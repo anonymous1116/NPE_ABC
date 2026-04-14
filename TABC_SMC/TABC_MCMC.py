a823df7fa6a6fee27cf790ad89168607058a070d
anonymous1116 <hhyun116@gmail.com>
2026-04-14 19:05:32 -0400

    Asdf

--- TABC_SMC/TABC_MCMC.py
+++ TABC_SMC/TABC_MCMC.py
@@ -128,7 +128,7 @@ def main(args):
 
         # ESS check
         if j % CHECK_EVERY == 0:
-            theta_chain = torch.cat(theta_list, dim=0).cpu().numpy()
+            theta_chain = torch.cat(theta_list, dim=1).cpu().numpy()
             print(theta_chain.shape)
             ess = az.ess(theta_chain[None, :, :], method="bulk")
             ess_min = ess.to_array().values.min()
