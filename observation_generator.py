c330578cdb8e3b88b4cb3c03fffc4e7e79e4fb04
anonymous1116 <hhyun116@gmail.com>
2026-04-14 10:39:59 -0400

    Asdf

--- observation_generator.py
+++ observation_generator.py
@@ -13,10 +13,12 @@ def main(args):
             sample = np.random.choice(np.arange(0, 10), size=10, replace=True)
             tmp = observation_lists("my_twomoons")
             tmp = np.array(tmp)
-            print(tmp[sample])
-            print(np.concatenate(tmp[sample],0))
-            #x0_list.append(tmp)
-
+            x0_list.append(np.concatenate(tmp[sample],0).tolist())
+        x0_list = torch.tensor(x0_list, dtype = torch.float32)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        torch.save(x0_list, f"{current_dir}/../depot_hyun/hyun/NPE_ABC/seeds/my_ten_twomoons_obs.pt")    
+        print(x0_list)
+        
 def get_args():
     # Create an argument parser
     parser = argparse.ArgumentParser(description="Run simulations and inference.")
