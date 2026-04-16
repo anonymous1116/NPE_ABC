3242ee3ecb9dc5baf5e01978e97b7c00a6302e8b
anonymous1116 <hhyun116@gmail.com>
2026-04-16 15:18:33 -0400

    Asdf

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=00:39:00
+#SBATCH --time=00:59:00
 #SBATCH --account=statdept
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=170G
@@ -29,7 +29,7 @@ seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
 L=10000000
 task="double_slcp_summary_transform2"
-num_training=300000 
+num_training=500000 
 tol=1e-5
 
 # Run the calibrate_amor.py
