f6a9fc34d606cebf5967515bf7da8239ea64edd6
anonymous1116 <hhyun116@gmail.com>
2026-04-16 15:19:13 -0400

    Asdf

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -29,7 +29,7 @@ seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
 L=10000000
 task="double_slcp_summary_transform2"
-num_training=1000000 
+num_training=2000000 
 tol=1e-5
 
 # Run the calibrate_amor.py
