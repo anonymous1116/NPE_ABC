8abf21ee9a18f0a1b86fed9dc907554705822973
anonymous1116 <hhyun116@gmail.com>
2026-04-19 11:43:50 -0400

    :asdf

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=statdept
 #SBATCH --time=2-12:00:00
 #SBATCH --qos=normal
-#SBATCH --array=8-10               # Create a job array with indices from 1 to 10
+#SBATCH --array=1-7               # Create a job array with indices from 1 to 10
 #SBATCH --output=NPE/NPE_nsf/output_log/output_log_%A_%a.log
 #SBATCH --error=NPE/NPE_nsf/output_log/error_log_%A_%a.txt
 
@@ -21,7 +21,7 @@ SLURM_SUBMIT_DIR=/home/hyun18/NPE_ABC
 cd $SLURM_SUBMIT_DIR
 
 # Define the starting point for seed
-seed_START=1
+seed_START=1s
 #TASK="MoG"
 
 # Get the current N_EPOCHS value based on the job array index
@@ -30,7 +30,7 @@ seeds=$((seed_START + SLURM_ARRAY_TASK_ID - 1))
 # Run the Python script with the specified N_EPOCHS value
 echo "Running with seed=$seeds"
 #python NPE_training/NPE_run_embed.py --task "my_five_twomoons_err2" --seed $seeds --cond_den "nsf" --num_training 1000000
-python NPE_training/FMPE_run.py --task "mog_10" --seed $seeds --num_training 3000000
+python NPE_training/NPE_run.py --task "mog_10" --seed $seeds --num_training 3000000
 #python NPE_training/NPE_run.py --task "mog_10" --seed $seeds --num_training 2000000
 #python NPE_training/NPE_run.py --task "double_slcp_summary_transform2" --seed $seed --num_training 300000 --cond_den "nsf"
 #python NPE_training/SNPE_run.py --task "two_moons" --seed 1 --num_training 1000 --cond_den "nsf" --x0_ind 0
