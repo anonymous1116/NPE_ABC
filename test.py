ccdcab383858f28400cf20815fca343f43879fe1
anonymous1116 <hhyun116@gmail.com>
2026-04-20 14:57:12 -0400

    asdf

---
+++ test.py
@@ -0,0 +1,20 @@
+import torch
+import simulator
+from simulator import Simulators, Priors, observation_lists, Bounds, true_Posteriors, simulator_slcp_distractors
+
+def main():
+    # Example usage of the Simulators class
+    #simulators = simulator.Simulators(task="slcp")
+    priors = simulator.Priors(task="slcp")
+    
+    # Sample theta from the prior
+    theta = priors.sample((100,))  # Sample 5 parameter sets
+
+    # Run the simulator
+    X = simulator_slcp_distractors(theta)
+
+    print("Sampled theta:")
+    print(theta)
+    print("Simulated data X:")
+    print(X)    
+    print(X.size())
