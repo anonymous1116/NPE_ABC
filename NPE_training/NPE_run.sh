832de165b5f586a6903ae8e02e90790a61014863
anonymous1116 <hhyun116@gmail.com>
2026-04-21 17:18:39 -0400

    :asdf

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=cpu
 #SBATCH --account=statdept
-#SBATCH --time=12:00:00
+#SBATCH --time=07:40:00
 #SBATCH --qos=normal
 #SBATCH --array=1-10               # Create a job array with indices from 1 to 10
 #SBATCH --output=NPE/NPE_nsf/output_log/output_log_%A_%a.log
