8571af02899244866134e93dfece74f30a31586a
anonymous1116 <hhyun116@gmail.com>
2026-04-24 14:28:58 -0400

    :asdf

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -3,9 +3,9 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=cpu
 #SBATCH --account=statdept
-#SBATCH --time=07:00:00
+#SBATCH --time=12-07:00:00
 #SBATCH --qos=normal
-#SBATCH --array=1-10               # Create a job array with indices from 1 to 10
+#SBATCH --array=1-2               # Create a job array with indices from 1 to 10
 #SBATCH --output=NPE/NPE_nsf/output_log/output_log_%A_%a.log
 #SBATCH --error=NPE/NPE_nsf/output_log/error_log_%A_%a.txt
 
@@ -30,8 +30,8 @@ seeds=$((seed_START + SLURM_ARRAY_TASK_ID - 1))
 # Run the Python script with the specified N_EPOCHS value
 echo "Running with seed=$seeds"
 #python NPE_training/NPE_run_embed.py --task "my_five_twomoons_err2" --seed $seeds --cond_den "nsf" --num_training 1000000
-#python NPE_training/NPSE_run.py --task "mog_10" --seed $seeds --num_training 300000
-python NPE_training/NPE_run.py --task "slcp" --seed $seeds --num_training 300000
+python NPE_training/NPSE_run.py --task "bernoulli_glm2" --seed $seeds --num_training 3000000
+#python NPE_training/NPE_run.py --task "bernoulli_glm20" --seed $seeds --num_training 300000
 #python NPE_training/NPE_run.py --task "double_slcp_summary_transform2" --seed $seed --num_training 300000 --cond_den "nsf"
 #python NPE_training/SNPE_run.py --task "two_moons" --seed 1 --num_training 1000 --cond_den "nsf" --x0_ind 0
 echo "## Run Completed for seed=$seeds ##"
