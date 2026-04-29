57b4a8cf9b859ad5c51451042e002fd7c8eee674
anonymous1116 <hhyun116@gmail.com>
2026-04-29 16:06:05 -0400

    :asdf

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=02:54:00
+#SBATCH --time=00:19:00
 #SBATCH --account=statdept
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=170G
