a9674d63fc208777b0bde4a48f1b6e82d5d1d39a
anonymous1116 <hhyun116@gmail.com>
2026-05-02 12:17:28 -0400

    :asdf

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=statdept
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=170G
-#SBATCH --qos=standby
+#SBATCH --qos=normal
 #SBATCH --partition=a10,a100-80gb
 #SBATCH --array=50-99
 #SBATCH --output=ABC_calibration/log/output_log_%A_%a.out
