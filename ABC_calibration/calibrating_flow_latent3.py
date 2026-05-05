377b22078407344ec5adc35d5a8cd7cbf86e6ddf
anonymous1116 <hhyun116@gmail.com>
2026-05-04 22:46:56 -0400

    latent.py

--- ABC_calibration/calibrating_flow_latent3.py
+++ ABC_calibration/calibrating_flow_latent3.py
@@ -139,7 +139,7 @@ def main(args):
     X_abc = torch.cat(X_abc)
     Y_abc = torch.cat(Y_abc)    
 
-    index_WABC = TABC_rejection(x0, X_abc, 0.1, density_estimator_npe, Y_abc.size(1), device, num_samples=100)
+    index_WABC = TABC_rejection(x0, X_abc, 0.1, density_estimator_npe, Y_abc.size(1), device, num_samples=1000)
     X_abc, Y_abc = X_abc[index_WABC], Y_abc[index_WABC]
 
     print("X_abc size", X_abc.size())
