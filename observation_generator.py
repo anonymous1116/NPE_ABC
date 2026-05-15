8062c19e4935d6344d844778fa8febc0065dd67a
anonymous1116 <hhyun116@gmail.com>
2026-05-15 13:29:37 -0400

    latent.py

--- observation_generator.py
+++ observation_generator.py
@@ -48,7 +48,7 @@ def main(args):
             noise = torch.randn( (noise_num,)) * 2.0 
             tmp = X_obs[j]
 
-            post_sample = true_posterior(torch.tensor(tmp)[None, :], n_samples=100_000, bounds=bounds)
+            post_sample = true_posterior(torch.tensor(tmp)[None, :], n_samples=10_000, bounds=bounds)
             torch.save(post_sample, f"{current_dir}/../depot_hyun/hyun/NPE_ABC/seeds/{args.task}_post_{j+1}.pt")
             tmp = torch.cat([tmp, noise])
             print(tmp)
