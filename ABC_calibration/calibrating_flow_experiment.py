71183da8adf6344f4b482010914e7bcb17416926
anonymous1116 <hhyun116@gmail.com>
2026-05-15 14:12:32 -0400

    latent.py

--- ABC_calibration/calibrating_flow_experiment.py
+++ ABC_calibration/calibrating_flow_experiment.py
@@ -102,7 +102,7 @@ def main(args):
         X_chunk_embed = embed(X_chunk.to(device))
     
 
-        index_ABC = ABC_rej2(x0_embed, X_chunk_embed, args.tol*10, device)
+        index_ABC = ABC_rej2(x0_embed, X_chunk_embed, args.tol*100, device)
         X_chunk, Y_chunk = X_chunk[index_ABC], Y_chunk[index_ABC]
         X_abc.append(X_chunk)
         Y_abc.append(Y_chunk)
@@ -113,7 +113,7 @@ def main(args):
     Y_abc = torch.cat(Y_abc)    
 
     X_abc_embed = embed(X_abc.to(device))
-    index_ABC = ABC_rej2(x0_embed, X_abc_embed, 0.1, device)
+    index_ABC = ABC_rej2(x0_embed, X_abc_embed, 0.01, device)
     X_abc, Y_abc = X_abc[index_ABC], Y_abc[index_ABC]
 
     print("X_abc size", X_abc.size())
@@ -136,7 +136,7 @@ def main(args):
     
     elapsed_time = end_time - start_time  # Calculate elapsed time
     
-    print("NABC sample size: ", new_theta.size())
+    print("TABC sample size: ", new_theta.size())
     results_size = min(10_000, new_theta.size(0))
 
     tmp = c2st(post_sample[:results_size].cpu(), new_theta[:results_size] )
