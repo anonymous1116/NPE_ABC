f9c08510359e3a74bc196eb690e38e67b680653f
anonymous1116 <hhyun116@gmail.com>
2026-05-18 14:16:22 -0400

    latent.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=170G
 #SBATCH --qos=standby
 #SBATCH --partition=a100-80gb,a10
-#SBATCH --array=50-99
+#SBATCH --array=0-99
 #SBATCH --output=ABC_calibration/log/output_log_%A_%a.out
 #SBATCH --error=ABC_calibration/log/error_log_%A_%a.txt
 
