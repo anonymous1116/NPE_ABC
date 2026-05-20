131be5a351543e3e93d90da72376609ec6904cb5
anonymous1116 <hhyun116@gmail.com>
2026-05-20 11:15:24 -0400

    latent.py

--- ABC_calibration/calibrating_CPU.sh
+++ ABC_calibration/calibrating_CPU.sh
@@ -38,7 +38,7 @@ echo "[$(date)] Starting job: x0_ind=$x0_ind, seed=$seed, L=$L"
 #python ABC_calibration/calibrating_flow.py --x0_ind $x0_ind --seed $seed --L $L --task $task --num_training $num_training --tol $tol
 #python ABC_calibration/calibrating_flow.py --x0_ind $x0_ind --seed $seed --L $L --task $task --num_training $num_training --tol $tol
 #python ABC_calibration/calibrating_flow_latent4.py --x0_ind $x0_ind --seed $seed --L $L --task $task --num_training $num_training --tol $tol 
-python ABC_calibration/calibrating_flow_latent_target.py --x0_ind $x0_ind --seed $seed --L $L --task $task --num_training $num_training --tol $tol
+python ABC_calibration/calibrating_flow_latent5.py --x0_ind $x0_ind --seed $seed --L $L --task $task --num_training $num_training --tol $tol
 #python ABC_calibration/calibrating_flow_latent_target.py --x0_ind $x0_ind --seed $seed --L $L --task $task --num_training $num_training --tol $tol
 
 echo "[$(date)] Job complete: x0_ind=$x0_ind, seed=$seed"
