fb5e94ebcfed3c76df14619c87f9d1c1a863303a
anonymous1116 <hhyun116@gmail.com>
2026-06-05 22:12:54 -0400

    latent.py

--- utils/get_measure_NABC.py
+++ utils/get_measure_NABC.py
@@ -76,4 +76,4 @@ if __name__ == "__main__":
     run_similiarity(args.task, args.measure, args.x0_ind, args.seed, args.post_n_samples, args.num_training, args.cond_den, args.method)
 
 
-#python utils/get_measure_NABC.py --task "slcp_summary_transform2" --measure "c2st" --x0_ind 1 --seed 1 --post_n_samples 10000 --num_training 1000 --cond_den "nsf" --method "FMPE"
+#python utils/get_measure_NABC.py --task "mog_5_nabc" --measure "c2st" --x0_ind 1 --seed 1 --post_n_samples 10000 --num_training 1000 --cond_den "nsf" --method "FMPE"
