875ed2d98ff3aff3e90563106d5e2e0bf7b88f61
anonymous1116 <hhyun116@gmail.com>
2026-06-08 16:26:43 -0400

    latent.py

--- ABC_calibration/calibrating_CPU.sh
+++ ABC_calibration/calibrating_CPU.sh
@@ -47,4 +47,4 @@ echo "[$(date)] Job complete: x0_ind=$x0_ind, seed=$seed"
 #conda activate /depot/wangxiao/apps/hyun18/NPE_NABC
 #python ABC_calibration/calibrating_flow_latent_target.py --x0_ind 9 --seed 1 --L 1000000 --task "my_five_twomoons_err90" --num_training 5000000 --tol 1e-2 
 #python ABC_calibration/calibrating_flow_experiment.py --x0_ind 1 --seed 1 --L 10000000 --task "my_five_twomoons" --num_training 300000 --tol 1e-3
-python ABC_calibration/calibrating_flow_experiment2.py --x0_ind 1 --seed 1 --task "my_five_twomoons" --num_training 3000000 
+#python ABC_calibration/calibrating_flow_experiment2.py --x0_ind 1 --seed 1 --task "my_five_twomoons" --num_training 3000000 
