a3b9837d258577a4b62b083bad074d85db6d2387
anonymous1116 <hhyun116@gmail.com>
2026-07-04 23:50:54 -0400

    latent.py

--- DP/DP_gibbs.sh
+++ DP/DP_gibbs.sh
@@ -29,5 +29,5 @@ seeds=$((seed_START + SLURM_ARRAY_TASK_ID - 1))
 
 # Run the Python script with the specified N_EPOCHS value
 echo "Running with seed=$seeds"
-python DP/DP_gibbs.py --task "table_dp_22" --seed $seeds
+python DP/DP_gibbs.py --task "table_dp_22" --i $seeds
 echo "## Run Completed for seed=$seeds ##"
