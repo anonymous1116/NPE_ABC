1f743672df56bc25f5fc2d5f042ff3ac02a5db4c
anonymous1116 <hhyun116@gmail.com>
2026-07-05 23:49:07 -0400

    latent.py

--- DP/calibrating_DP.py
+++ DP/calibrating_DP.py
@@ -87,8 +87,10 @@ def truncated_dirichletK_stick(L, K, lower, upper, *, device=None, dtype=torch.f
         # Fix infeasible intervals numerically
         bk = torch.maximum(bk, ak + eps)
 
-       # if torch.any(ak >= bk):
-       #     raise ValueError(f"Infeasible bounds at step k={k}.")
+        if torch.any(ak >= bk):
+            print(f"ak: {ak}")
+            print(f"bk: {bk}")
+            raise ValueError(f"Infeasible bounds at step k={k}.")
 
         # V_k ~ Beta(1, K-k-1) truncated to [ak, bk]
         beta_param = float(K - k - 1)
