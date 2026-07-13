1c566577f30e485aab86500bbc141bc4d0b4a9da
anonymous1116 <hhyun116@gmail.com>
2026-07-13 10:47:04 -0400

    latent.py

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -34,7 +34,7 @@ echo "Running with seed=$seeds"
 python NPE_training/NPE_DP_run.py --task "table_dp_55" --seed $seeds --num_training 500000 --method "NPE"
 #python NPE_training/NPE_run.py --task "double_slcp_summary_transform2" --seed $seed --num_training 300000 --cond_den "nsf"
 #python NPE_training/SNPE_run.py --task "two_moons" --seed 1 --num_training 1000 --cond_den "nsf" --x0_ind 0
-#python NPE_training/NPE_DP_run.py --task "table_dp_22" --seed 1 --num_training 1000 --method "FMPE"
+#python NPE_training/NPE_DP_run.py --task "table_dp_66" --seed 1 --num_training 1000 --method "NPE"
 
 echo "## Run Completed for seed=$seeds ##"
 
