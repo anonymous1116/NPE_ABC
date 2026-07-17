bca7002e90c2ffa4ccbc233431ddd663d706901f
anonymous1116 <hhyun116@gmail.com>
2026-07-16 20:11:07 -0400

    latent.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -5,8 +5,8 @@
 #SBATCH --account=statdept
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=170G
-#SBATCH --qos=standby
-#SBATCH --partition=a10,a100-80gb
+#SBATCH --qos=normal
+#SBATCH --partition=a10
 #SBATCH --array=0-99
 #SBATCH --output=ABC_calibration/log/output_log_%A_%a.out
 #SBATCH --error=ABC_calibration/log/error_log_%A_%a.txt
@@ -29,8 +29,8 @@ seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
 
 L=1000000000
-task="table_dp_55"
-num_training=1000000 
+task="table_dp_66"
+num_training=500000 
 tol=1e-5
 #cdim=10
 # Run the calibrate_amor.py
