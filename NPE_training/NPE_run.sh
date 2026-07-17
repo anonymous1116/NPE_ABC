5a994565f96194bea3e0ba44a35bfb9039500293
anonymous1116 <hhyun116@gmail.com>
2026-07-16 20:00:43 -0400

    latent.py

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -32,7 +32,7 @@ echo "Running with seed=$seeds"
 #python NPE_training/NPE_run_embed.py --task "slcp_distractors" --seed $seeds --cond_den "nsf" --num_training 1000000 --cdim 5
 #python NPE_training/NPSE_run.py --task "bernoulli_glm2" --seed $seeds --num_training 1500000
 #python NPE_training/NPE_DP_run.py --task "table_dp_66" --seed $seeds --num_training 100000 --method "NPE"
-python NPE_training/NPE_run.py --task "my_five_twomoons" --seed $seeds --num_training 3000000 --method "NPSE_vp"
+python NPE_training/NPE_run.py --task "bernoulli_glm2" --seed $seeds --num_training 300000 --method "NPSE"
 #python NPE_training/SNPE_run.py --task "double_slcp_summary_transform2" --seed 1 --num_training 1000 --cond_den "nsf" --x0_ind 0
 #python NPE_training/NPE_DP_run.py --task "table_dp_66" --seed 1 --num_training 1000 --method "NPE"
 
