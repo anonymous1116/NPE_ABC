d891ecdf3f3d0ea0d38842be5a50bd9586a1d45b
anonymous1116 <hhyun116@gmail.com>
2026-07-24 15:09:02 -0400

    latent.py

--- ABC_calibration/calibrating.sh
+++ ABC_calibration/calibrating.sh
@@ -29,8 +29,8 @@ seed=$((SLURM_ARRAY_TASK_ID / 10 + 1))
 #L=100000000
 
 L=1000000000
-task="double_slcp_summary_transform2"
-num_training=500000 
+task="bernoulli_glm2"
+num_training=2000000 
 tol=1e-5
 #cdim=10
 # Run the calibrate_amor.py
