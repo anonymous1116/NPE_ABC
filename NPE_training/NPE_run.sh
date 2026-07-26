00743c364ca1edcd065fcd6425e9514d679f712c
anonymous1116 <hhyun116@gmail.com>
2026-07-26 14:04:29 -0400

    latent.py

--- NPE_training/NPE_run.sh
+++ NPE_training/NPE_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=8
 #SBATCH --partition=cpu
 #SBATCH --account=statdept
 #SBATCH --time=5-04:00:00
@@ -31,8 +31,8 @@ seeds=$((seed_START + SLURM_ARRAY_TASK_ID - 1))
 echo "Running with seed=$seeds"
 #python NPE_training/NPE_run_embed.py --task "slcp_distractors" --seed $seeds --cond_den "nsf" --num_training 1000000 --cdim 5
 #python NPE_training/NPSE_run.py --task "bernoulli_glm2" --seed $seeds --num_training 1500000
-#python NPE_training/NPE_DP_run.py --task "table_dp_66" --seed $seeds --num_training 2000000 --method "NPE"
-python NPE_training/NPE_run.py --task "bernoulli_glm2" --seed $seeds --num_training 3000000 --method "NPSE_vp"
+python NPE_training/NPE_DP_run.py --task "table_dp_66" --seed $seeds --num_training 5000000 --method "NPE"
+#python NPE_training/NPE_run.py --task "bernoulli_glm2" --seed $seeds --num_training 3000000 --method "NPSE_vp"
 #python NPE_training/SNPE_run.py --task "double_slcp_summary_transform2" --seed 1 --num_training 1000 --cond_den "nsf" --x0_ind 0
 #python NPE_training/NPE_DP_run.py --task "table_dp_66" --seed 1 --num_training 1000 --method "NPE"
 
