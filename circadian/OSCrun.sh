ec389601ad84a6882aac11e858340bd6a42422f9
anonymous1116 <hhyun116@gmail.com>
2026-09-09 14:31:48 -0400

    latent.py

--- circadian/OSCrun.sh
+++ circadian/OSCrun.sh
@@ -13,10 +13,10 @@
 mkdir -p circadian/output_log
 
 SLURM_SUBMIT_DIR=/users/PES0984/hhyun116/NPE_ABC
-cd $SLURM_SUBMIT_DIR
 
 module load miniconda3/24.1.2-py310
 source activate BayesCalib
+#export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 cd $SLURM_SUBMIT_DIR                  # run from wherever you submitted the job
 python circadian/training.py --task "circadian" --method NPE --num_training 10000
