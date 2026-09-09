155d9baa99e4e2e135d1859050228464a54abd95
anonymous1116 <hhyun116@gmail.com>
2026-09-09 16:46:48 -0400

    latent.py

--- circadian/training.py
+++ circadian/training.py
@@ -90,7 +90,7 @@ def main(args):
     #theta, X = filter_bottom_99(theta, X)
     #print(f"After filtering, theta shape: {theta.shape}, X shape: {X.shape}")
 
-    X_np = X.cpu().numpy()  # move off GPU, convert to numpy for plotting
+    X_np = X_abc.cpu().numpy()  # move off GPU, convert to numpy for plotting
     n_freqs = X_np.shape[1]
 
     fig, axes = plt.subplots(1, n_freqs, figsize=(4 * n_freqs, 4))
