9b3563529c80c1a35ca5e9a2549e17470c6a8461
anonymous1116 <hhyun116@gmail.com>
2026-09-09 14:13:47 -0400

    latent.py

--- circadian/OSCrun.sh
+++ circadian/OSCrun.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #SBATCH --account=PES0984            # replace with your actual project account
 #SBATCH --job-name=NPE_ABC
-#SBATCH --output=%j.out            # %x = job name, %j = job ID
-#SBATCH --error=%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --gpus-per-node=1
 #SBATCH --partition=debug
 #SBATCH --time=00:30:00
+#SBATCH --output=circadian/output_log/%j.out            # %x = job name, %j = job ID
+#SBATCH --error=circadian/output_log/%j.err
+
+mkdir -p circadian/output_log
+
+SLURM_SUBMIT_DIR=/home/hhyun116/NPE_ABC
+cd $SLURM_SUBMIT_DIR
 
 module load miniconda3/24.1.2-py310
 source activate BayesCalib
 
 cd $SLURM_SUBMIT_DIR                  # run from wherever you submitted the job
-python training.py
+python circadian/training.py --task "circadian" --method NPE --num_training 10000
