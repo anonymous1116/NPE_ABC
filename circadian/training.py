3f9bc6c116a8356aed3eb01d6b7b71accef1a709
anonymous1116 <hhyun116@gmail.com>
2026-09-09 16:58:03 -0400

    latent.py

--- circadian/training.py
+++ circadian/training.py
@@ -110,55 +110,54 @@ def main(args):
     plt.savefig("circadian/X_distributions.png", dpi=150)
     plt.show()
 
-    if 0:
-        print(torch.max(X, dim=0).values, torch.min(X, dim=0).values)
-        
-        # Create inference object
-        if args.method == "FMPE":
-            inference = FMPE(prior=priors)
-        elif args.method == "NPSE":
-            inference = NPSE(prior = priors)
-        elif args.method == "NPSE_vp":
-            inference = NPSE(prior = priors, sde_type="vp")
-        else:
-            inference = NPE(prior=priors, density_estimator="nsf")
-        inference = inference.append_simulations(theta, X)
+    print(torch.max(X_abc, dim=0).values, torch.min(X_abc, dim=0).values)
+    
+    # Create inference object
+    if args.method == "FMPE":
+        inference = FMPE(prior=priors)
+    elif args.method == "NPSE":
+        inference = NPSE(prior = priors)
+    elif args.method == "NPSE_vp":
+        inference = NPSE(prior = priors, sde_type="vp")
+    else:
+        inference = NPE(prior=priors, density_estimator="nsf")
+    inference = inference.append_simulations(Y_abc, X_abc)
 
-        # Train the density estimator and build the posterior
-        print(f"training_start")
-        start_time = time.time()  # Start time
-        density_estimator = inference.train()
-        end_time = time.time()  # End timer
+    # Train the density estimator and build the posterior
+    print(f"training_start")
+    start_time = time.time()  # Start time
+    density_estimator = inference.train()
+    end_time = time.time()  # End timer
 
-        elapsed_time = end_time - start_time  # Calculate elapsed time
-        print(f"Training completed in {elapsed_time:.2f} seconds")
-        
-        # Define the output directory
-        output_dir = f"circadian/nets_depot/{args.method}/{args.task}/J_{int(args.num_training/1000)}K"
-        
-        # Create the directory if it doesn't exist
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-            print(f"Directory '{output_dir}' created.")
-        else:
-            print(f"Directory '{output_dir}' already exists.")
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    print(f"Training completed in {elapsed_time:.2f} seconds")
+    
+    # Define the output directory
+    output_dir = f"circadian/nets_depot/{args.method}/{args.task}/J_{int(args.num_training/1000)}K"
+    
+    # Create the directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        print(f"Directory '{output_dir}' created.")
+    else:
+        print(f"Directory '{output_dir}' already exists.")
 
-        # Save the inference object using pickle in the specified directory
-        # Save the inference object and elapsed time using pickle in the specified directory
-        output_file_path = os.path.join(output_dir, f"{args.task}_{args.seed}.pkl")
+    # Save the inference object using pickle in the specified directory
+    # Save the inference object and elapsed time using pickle in the specified directory
+    output_file_path = os.path.join(output_dir, f"{args.task}_{args.seed}_{args.x0_ind}.pkl")
+    
+    if args.method in ["NPSE", "NPSE_vp"]:
+        torch.save({
+            'net_state_dict': density_estimator.net.state_dict(),
+            'embedding_net_state_dict': density_estimator._embedding_net.state_dict(),
+            'full_state_dict': density_estimator.state_dict(),  # includes all buffers
+            'elapsed_time': elapsed_time
+        }, output_file_path.replace('.pkl', '.pt'))
+    else:
+        with open(output_file_path, 'wb') as f:
+            pickle.dump({'density_estimator': density_estimator, 'posterior': inference.build_posterior(density_estimator), 'elapsed_time': elapsed_time}, f)
         
-        if args.method in ["NPSE", "NPSE_vp"]:
-            torch.save({
-                'net_state_dict': density_estimator.net.state_dict(),
-                'embedding_net_state_dict': density_estimator._embedding_net.state_dict(),
-                'full_state_dict': density_estimator.state_dict(),  # includes all buffers
-                'elapsed_time': elapsed_time
-            }, output_file_path.replace('.pkl', '.pt'))
-        else:
-            with open(output_file_path, 'wb') as f:
-                pickle.dump({'density_estimator': density_estimator, 'posterior': inference.build_posterior(density_estimator), 'elapsed_time': elapsed_time}, f)
-            
-        print(f"Saved inference object and elapsed time to '{output_file_path}'.")
+    print(f"Saved inference object and elapsed time to '{output_file_path}'.")
 
 def get_args():
     # Create an argument parser
