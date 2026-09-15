424699a13760c6a02d5472254412f6a220a52179
anonymous1116 <hhyun116@gmail.com>
2026-09-15 17:28:43 -0400

    latent.py

--- observation_generator.py
+++ observation_generator.py
@@ -202,9 +202,9 @@ def main(args):
         fold_num = int(args.task.replace("fold", ""))
         random.seed(2826)
         torch.manual_seed(2826)
+        print(x0_list)
         x0_list = [[5.0]]*fold_num
         x0_list = torch.tensor(x0_list, dtype = torch.float32)
-        print(x0_list)
         torch.save(x0_list, f"{current_dir}/../depot_hyun/hyun/NPE_ABC/seeds/{args.task}_obs.pt")           
         from simulator import fold_posterior_sample_rejection
         post = []
