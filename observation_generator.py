cfd7aa7dc5b4d731ac33e8f080d6a5070257a3ef
anonymous1116 <hhyun116@gmail.com>
2026-09-15 22:09:34 -0400

    latent.py

--- observation_generator.py
+++ observation_generator.py
@@ -210,9 +210,9 @@ def main(args):
         for j in range(fold_num):
             post_onedim = fold_posterior_sample_rejection(5.0, n =10_000)
             post.append(post_onedim)
-        post_sample = torch.column_stack(post)
-        print(post_sample.size())
-        torch.save(post_sample, f"{current_dir}/../depot_hyun/hyun/NPE_ABC/seeds/{args.task}_post_1.pt")    
+        post = torch.column_stack(post)
+        print(post.size())
+        torch.save(post, f"{current_dir}/../depot_hyun/hyun/NPE_ABC/seeds/{args.task}_post_1.pt")    
     else:
         print("Task not recognized.")
 
